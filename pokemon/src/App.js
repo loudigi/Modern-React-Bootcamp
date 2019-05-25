a55aa82a0ac735f267a285f058b544acc224f853
Cabrera <lcabrera@forestcity.net>
2019-05-24 22:03:07 -0400

    Components and styles added

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Pokecard from "./Pokecard";
+import Pokedex from "./Pokedex";
 
 function App() {
   return (
     <div className="App">
-      <Pokecard />
+      <Pokedex />
     </div>
   );
 }
